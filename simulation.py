7192daf85c1bd828d5d2fc612148a29e02a5435c
Robert Ross Wardrup <minorsecond@gmail.com>
2016-03-14 07:30:03 -0500

    Write vector population output file. Display percentage complete for output file writing.

--- simulation.py
+++ simulation.py
@@ -1,5 +1,10 @@
 import csv
+import os.path
 import random
+from sys import exit as die
+from time import sleep
+
+import numpy as np
 
 # Simulation parameters
 days_to_run = 999
@@ -21,9 +26,19 @@ initial_exposed = 0
 initial_infected = 1
 
 # Mosquito population parameters
-mosquito_susceptible = initial_susceptible * 10
+mosquito_susceptible = 1000000
 mosquito_exposed = 0
 mosquito_init_infectd = 0
+
+
+def prompt(question):
+    reply = str(input(question + ' (y/n): ')).lower().strip()
+    if reply[0] == 'y':
+        return True
+    if reply[0] == 'n':
+        return False
+    else:
+        return question("Try again: ")
 
 
 def build_population(N):
@@ -47,8 +62,8 @@ def build_population(N):
             'dayOfRec': 0,
             'recState': 0,
             'resistant': False,
-            'x': random.uniform(689141, 737293),  # Extents for Tarrant county, TX
-            'y': random.uniform(2098719, 2147597)
+            'x': np.random.uniform(689141.000, 737293.000),  # Extents for Tarrant county, TX
+            'y': np.random.uniform(2098719.000, 2147597.000)
 
         }) for i in range(N)
     )
@@ -72,11 +87,13 @@ def build_vectors(N):
 
     vector_population = dict(
         (i, {
-            'range': random.normalvariate(0, 500),  # 500 meters or so
+            'range': random.uniform(0, 500),  # 500 meters or so
             'lifetime': random.uniform(0, 14),  # in days
             'susceptible': 'True',
             'exposed': 'False',
             'infected': 'False',
+            'x': np.random.uniform(689141.000, 737293.000),  # Extents for Tarrant county, TX
+            'y': np.random.uniform(2098719.000, 2147597.000)
         }) for i in range(N)
     )
 
@@ -94,13 +111,54 @@ def writer(filename, line):
         writer.writerow(line)
 
 
-def main():
-    population_structure_file = input("Population File Output CSV: ")
+def check_if_file_exists(file):
+    """
+    Checks if file exists and prompts user for action
+    :param file: File path
+    :return:
+    """
+
+    if os.path.isfile(file):
+        if prompt("File {0} exists: overwrite?".format(file)):
+            try:
+                os.remove(file)
+            except OSError:
+                raise OSError
+
+        elif prompt("Create backup of {0} named {1}? ".format(file, file + '.bk')):
+            new_file = file + '.bk'
+            os.rename(file, new_file)
+
+        else:
+            print("Take care of existing files and try again. I'm leaving you. Bye.")
+            die()
+
+
+def output_status(n, total):
+    """
+    Gets percentage of file output to print to screen
+    :param n:
+    :param total:
+    :return:
+    """
+
+    return ((n / total) * 100)
+
+
+def build_population_files(directory):
+    population_structure_file = os.path.join(directory, 'human_population.csv')
+    check_if_file_exists(population_structure_file)
+
+    vector_structure_file = os.path.join(directory, 'vector_population.csv')
+    check_if_file_exists(vector_structure_file)
+
 
     pregnancy_eligible = 0
     pregnant_count = 0
     header_count = 0
+    run_count = 0
 
+    # Print population structure info
     population = (build_population(initial_susceptible))
     for i in population:
         age = population[i].get('age')
@@ -123,11 +181,50 @@ def main():
             lineOut = [i, age, sex, pregnant, x, y]
         writer(population_structure_file, lineOut)
 
+        if run_count == 0:
+            print('Building population file: {0}% Complete'.format(round(output_status(run_count, len(population)))))
+
+        run_count += 1
+        if run_count % (len(population) / 100) == 0:
+            print('Building population file: {0}% Complete'.format(round(output_status(run_count, len(population)))))
+
+    # Print vector structure info
+
+    print("Building vector population")
+    sleep(5)
+
+    vector = (build_vectors(mosquito_susceptible))
+    run_count = 0
+    header_count = 0
+
+    for i in vector:
+        range = vector[i].get('range')
+        lifetime = vector[i].get('lifetime')
+        x = vector[i].get('x')
+        y = vector[i].get('y')
+
+        if header_count == 0:
+            lineOut = ['Vector ID', 'Range', 'Lifetime', 'x', 'y']
+            header_count = 1
+
+        else:
+            lineOut = [i, range, lifetime, x, y]
+        writer(vector_structure_file, lineOut)
+
+        if run_count == 0:
+            print('Building vector file: {0}% Complete'.format(round(output_status(run_count, len(population)))))
+
+        run_count += 1
+        if run_count % (len(vector) / 100) == 0:
+            print('Building vector file: {0}% Complete'.format(round(output_status(run_count, len(vector)))))
+
+    # stats
+
     if pregnant_count > 0:
         pregnant_percentage = (pregnant_count / pregnancy_eligible) * 100
     else:
-        pregnant_percentage = "No pregnancies"
-    print("Percent of eligible women pregnant: {0}".format(pregnant_percentage))
+        pregnant_percentage = "\nNo pregnancies"
+    print("\nPercent of eligible women pregnant: {0}".format(pregnant_percentage))
 
 
 def simulation():
@@ -137,6 +234,15 @@ def simulation():
     """
 
     day_of_infection = 0
+
+
+def main():
+    working_directory = input("Which directory should I place output data?: ")
+
+    if not os.path.exists(working_directory):
+        os.makedirs(working_directory)
+
+    build_population_files(working_directory)
 
 
 if __name__ == '__main__':
