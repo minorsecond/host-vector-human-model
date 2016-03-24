befc08b2319c4cb93d792e789abe961da548e3d2
Robert Ross Wardrup <minorsecond@gmail.com>
2016-03-24 15:50:06 -0500

    Added shape data and SQL queries.

---
+++ sql/vector-host-linker.sql
@@ -0,0 +1,8 @@
+﻿CREATE INDEX host_gix ON public."Humans" USING GIST (geom);
+CREATE INDEX vector_gix ON Vectors USING GIST (geom);
+DELETE FROM vector_human_links;
+
+INSERT INTO vector_human_links (human_id, vector_id, distance)
+SELECT h.id, v.id, ST_Distance(h.geom, v.geom)
+	FROM public."Humans" h
+	JOIN Vectors v on ST_DWithin(v.geom, h.geom, v.vector_range);
