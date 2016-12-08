57d440385b2e83911f14b67682524acfb1bed3b2
Ross Wardrup <rwardrup@imspatial.me>
2016-12-08 05:11:54 -0600

    Added a new conda-setup script

---
+++ conda-setup.sh
@@ -0,0 +1,12 @@
+echo "Creating Conda environment 'HostVectorModel'"
+conda create -y --name HostVectorModel python=3.4
+
+echo "Activating environment and installing required modules"
+source activate HostVectorModel
+conda install -y numpy
+conda install -y sqlalchemy
+#conda install -y configparser
+#conda install -y -c davidbgonzalez geoalchemy2=0.2.4
+pip install geoalchemy2
+pip install configparser
+pip install psycopg2
